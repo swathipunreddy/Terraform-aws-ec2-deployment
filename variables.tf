1a266dd5380076154035593de3e717929e5456ea
Swathi Punreddy <125949194+swathipunreddy@users.noreply.github.com>
2024-03-29 12:18:56 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+  description = "this is ubuntu ami id"
+  default     = "ami-053b0d53c279acc90"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    "name" = "Shubham's ec2"
+  }
+}
